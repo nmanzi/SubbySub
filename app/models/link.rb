0cba32a3abe9615319f379b4770b247e69480cdc
Nathan Manzi <nathan@nmanzi.com>
2011-10-05 20:29:35 +0800

    Added validations to links

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,4 @@
 class Link < ActiveRecord::Base
+  validates :subdomain, :exclusion => { :in => %w(www us ca jp), :message => "%{value} is reserved." }, :uniqueness => true
+  validates :url, :presence => true
 end
