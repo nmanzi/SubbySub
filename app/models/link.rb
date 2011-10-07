dda1fce01a5dbd254f4bb8254d8f840000ab361c
Nathan Manzi <nathan@nmanzi.com>
2011-10-07 20:47:31 +0800

    Added Link.total_visits class method

--- app/models/link.rb
+++ app/models/link.rb
@@ -6,6 +6,14 @@ class Link < ActiveRecord::Base
   validates :url, :presence => true, :url => true
   before_save :set_defaults
   
+  def self.total_visits
+    total_visits = 0
+    self.all.each do |item|
+      total_visits += item.visits
+    end
+    return total_visits
+  end
+  
   def set_defaults
     self.visits ||= 0
   end
