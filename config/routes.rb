ac4d91cbe97f4631ac1336bc274713335ad9e8b8
Nathan Manzi <nathan@nmanzi.com>
2011-10-07 12:45:12 +0800

    Controller 'sessions' and assoc views finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ require 'subdomain'
 
 Subbysub::Application.routes.draw do
   resources :links
+  resources :sessions
+  get 'login' => 'sessions#new', :as => 'login'
+  get 'logout' => 'sessions#destroy', :as => 'logout'
   constraints(Subdomain) do
     match '/' => 'links#show'
   end
