48c93dfc6359be97b554386ee17ff5fb220fd90b
Nathan Manzi <nathan@nmanzi.com>
2011-10-10 07:38:23 +0800

    Subdomain string validation added. Closes #1

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,7 +2,8 @@ class Link < ActiveRecord::Base
   validates :subdomain, 
             :exclusion => { :in => %w(www us ca jp), :message => "%{value} is reserved." }, 
             :uniqueness => true, 
-            :presence => true
+            :presence => true,
+            :format => { :with => /\A([a-zA-Z]|\d)*\Z/ }
   validates :url, :presence => true, :url => true
   before_save :set_defaults
   after_initialize :set_suggested_subdomain
